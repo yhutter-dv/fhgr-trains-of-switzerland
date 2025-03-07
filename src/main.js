1248e676337ee2cea1a04249e4e2346dc34c257a
yhutter <hutteryannick@gmail.com>
2025-03-07 09:48:35 +0100

    Display train tracks and station names

---
+++ src/main.js
@@ -0,0 +1,60 @@
+"use strict";
+import * as d3 from "d3";
+
+window.onload = () => main();
+
+async function main() {
+    const train_network_config = {
+        width: 960,
+        height: 500,
+        center: [8.2275, 46.8182],
+        scale: 8000
+    };
+    await create_train_network_map(train_network_config);
+}
+
+async function load_train_network_data() {
+    const file_path = "./sbb_train_network.geojson";
+    const response = await fetch(file_path);
+    const data = await response.json();
+    return data.features;
+}
+
+function add_train_tracks(svg, projection, data) {
+    const path = d3.geoPath().projection(projection);
+    svg.selectAll("path")
+        .data(data)
+        .enter()
+        .append("path")
+        .attr("class", "train-track")
+        .attr("d", path);
+}
+
+function add_train_stations(svg, projection, data) {
+    svg.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("class", "station-name")
+        .attr("x", (d) => projection([d.properties["geo_point_2d"].lon, d.properties["geo_point_2d"].lat])[0])
+        .attr("y", (d) => projection([d.properties["geo_point_2d"].lon, d.properties["geo_point_2d"].lat])[1])
+        .text((d) => d.properties["linienname"]);
+}
+
+
+async function create_train_network_map(config) {
+    const train_network_data = await load_train_network_data();
+    
+    const svg = d3.select("body")
+                    .append("svg")
+                    .attr("width", config.width)
+                    .attr("height", config.height);
+
+    const projection = d3.geoMercator()
+                        .center(config.center)
+                        .translate([config.width / 2, config.height / 2])
+                        .scale(config.scale);
+
+    add_train_tracks(svg, projection, train_network_data);
+    add_train_stations(svg, projection, train_network_data);
+}
